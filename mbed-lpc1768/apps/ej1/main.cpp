64975795834019f8f94e5ae230c2f62564c5fdb0
ValentinoVerdi <valenverdi17@gmail.com>
2025-11-12 17:31:36 -0300

    Implementado contador binario ej1

--- mbed-lpc1768/apps/ej1/main.cpp
+++ mbed-lpc1768/apps/ej1/main.cpp
@@ -1,14 +1,23 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
 
 int main()
 {
-    while(1)
-    {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
-    }
+	while(1)
+	    {
+		int cont=1;
+
+	        for (int i = 0; i <= 15; i++)
+	        {
+	            led1 = (i >> 0) & 1;
+	            led2 = (i >> 1) & 1;
+	            led3 = (i >> 2) & 1;
+	            led4 = (i >> 3) & 1;
+	            wait_us(1000000);
+	        }
+	    }
 }

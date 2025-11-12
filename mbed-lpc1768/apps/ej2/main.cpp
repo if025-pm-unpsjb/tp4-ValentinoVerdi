aba0d88a785d8c35ecba4bf7de317c7e8b1472d1
ValentinoVerdi <valenverdi17@gmail.com>
2025-11-12 17:48:57 -0300

    Implementado cambiar intensidad del brillo de ej2

--- mbed-lpc1768/apps/ej2/main.cpp
+++ mbed-lpc1768/apps/ej2/main.cpp
@@ -1,14 +1,33 @@
 #include "mbed.h"
+PwmOut led1(LED1);
+PwmOut led2(LED2);
+PwmOut led3(LED3);
+PwmOut led4(LED4);
 
-DigitalOut led1(LED1);
+void cambiarIntensidad(PwmOut& led)
+{
+    for (float brillo = 0.0f; brillo<= 1.0f; brillo+= 0.05f)
+    {
+        led = brillo;
+        wait_us(25000);
+    }
+
+    for (float brillo = 1.0f; brillo >= 0.0f; brillo -= 0.05f)
+    {
+        led = brillo;
+        wait_us(25000);
+    }
+    led = 0.0f;
+}
+
 
 int main()
 {
     while(1)
     {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
+    	cambiarIntensidad(led1);
+    	cambiarIntensidad(led2);
+    	cambiarIntensidad(led3);
+    	cambiarIntensidad(led4);
     }
 }

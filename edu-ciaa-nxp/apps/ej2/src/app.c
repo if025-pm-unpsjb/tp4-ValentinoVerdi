663928e62a71a5baf03407d6f029b7906b23ab1b
ValentinoVerdi <valenverdi17@gmail.com>
2025-11-13 17:54:00 -0300

    implementacion ej2 edu-ciaa

--- edu-ciaa-nxp/apps/ej2/src/app.c
+++ edu-ciaa-nxp/apps/ej2/src/app.c
@@ -1,22 +1,37 @@
-/*
- * main.c
- *
- *  Created on: 13 oct. 2022
- *      Author: fep
- */
-
-#include "app.h"         // <= Su propia cabecera (opcional)
-#include "sapi.h"        // <= Biblioteca sAPI
+#include "sapi.h"
 
 int main( void )
 {
-        // Inicializar y configurar la placa de desarrollo
-        boardConfig();
+   boardConfig();
+   uartConfig( UART_USB, 115200 );
 
-        while( TRUE ) {
-                gpioWrite( CIAA_BOARD_LED, 1 );
-                delay(1000);
-                gpioWrite( CIAA_BOARD_LED, 0 );
-                delay(1000);
-        }
+   gpioMap_t teclas[4] = { TEC1, TEC2, TEC3, TEC4 };
+   gpioMap_t leds[4]   = { CIAA_BOARD_LED, LED1, LED2, LED3 };
+   bool_t estadoAnterior[4] = { ON, ON, ON, ON };
+   bool_t estadoActual;
+
+   int i;
+
+   while( TRUE ) {
+
+      for( i = 0; i < 4; i++ ) {
+
+         estadoActual = gpioRead( teclas[i] );
+
+         if( estadoActual == OFF && estadoAnterior[i] == ON ) {
+
+            gpioToggle( leds[i] );
+
+            printf( "BOTON%d\r\n", i + 1 );
+
+            delay(50);
+         }
+
+         estadoAnterior[i] = estadoActual;
+      }
+
+      delay(10);
+   }
+
+   return 0;
 }

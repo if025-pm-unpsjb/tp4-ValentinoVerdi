244e35708d39cf71fe6ce90982ec766858d1ca07
ValentinoVerdi <valenverdi17@gmail.com>
2025-11-13 16:02:16 -0300

    implementacion ej4 mbed

--- mbed-lpc1768/apps/ej4/main.cpp
+++ mbed-lpc1768/apps/ej4/main.cpp
@@ -1,14 +1,45 @@
 #include "mbed.h"
+#include <map>
+
+Serial pc(USBTX, USBRX);
 
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+
+std::map<char, DigitalOut*> ledMap;
 
 int main()
 {
+    led1 = 0;
+    led2 = 0;
+    led3 = 0;
+    led4 = 0;
+
+    ledMap['1'] = &led1;
+    ledMap['2'] = &led2;
+    ledMap['3'] = &led3;
+    ledMap['4'] = &led4;
+
+    DigitalOut* activeLed = &led1;
+
     while(1)
     {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
+
+        if (pc.readable()) {
+            char c = pc.getc();
+
+            pc.putc(c);
+            pc.printf(" ");
+
+            if (ledMap.find(c) != ledMap.end()) {
+                *activeLed = 0;
+                activeLed = ledMap[c];
+            }
+        }
+        *activeLed = !(*activeLed);
+
+        wait_us(250000);
     }
 }

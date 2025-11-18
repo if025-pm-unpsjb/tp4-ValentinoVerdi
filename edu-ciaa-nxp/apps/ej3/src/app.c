1b9375705f451b481d231679847297adcba69438
ValentinoVerdi <valenverdi17@gmail.com>
2025-11-17 21:02:34 -0300

    implementacion ej3 edu-ciaa

--- edu-ciaa-nxp/apps/ej3/src/app.c
+++ edu-ciaa-nxp/apps/ej3/src/app.c
@@ -1,22 +1,62 @@
-/*
- * main.c
- *
- *  Created on: 13 oct. 2022
- *      Author: fep
- */
-
-#include "app.h"         // <= Su propia cabecera (opcional)
-#include "sapi.h"        // <= Biblioteca sAPI
+#include "sapi.h"
 
 int main( void )
 {
-        // Inicializar y configurar la placa de desarrollo
-        boardConfig();
+   boardConfig();
+   uartConfig( UART_USB, 115200 );
 
-        while( TRUE ) {
-                gpioWrite( CIAA_BOARD_LED, 1 );
-                delay(1000);
-                gpioWrite( CIAA_BOARD_LED, 0 );
-                delay(1000);
-        }
+   bool_t estadoRojo  = OFF;
+   bool_t estadoVerde = OFF;
+   bool_t estadoAzul  = OFF;
+   bool_t estadoMaestro = ON;
+   bool_t tec1_ant = ON, tec2_ant = ON, tec3_ant = ON, tec4_ant = ON;
+   bool_t tec1_act, tec2_act, tec3_act, tec4_act;
+
+   while( TRUE ) {
+      tec1_act = gpioRead( TEC1 );
+      tec2_act = gpioRead( TEC2 );
+      tec3_act = gpioRead( TEC3 );
+      tec4_act = gpioRead( TEC4 );
+
+      if( tec1_act == OFF && tec1_ant == ON ) {
+         estadoMaestro = !estadoMaestro;
+         if (estadoMaestro == ON) {
+             printf( "RGB: ON\r\n" );
+         } else {
+             printf( "RGB: OFF\r\n" );
+         }
+         delay(50);
+      }
+
+      if( tec2_act == OFF && tec2_ant == ON ) {
+         estadoRojo = !estadoRojo;
+         printf( "Rojo: %d\r\n", estadoRojo );
+         delay(50);
+      }
+
+      if( tec3_act == OFF && tec3_ant == ON ) {
+         estadoVerde = !estadoVerde;
+         printf( "Verde: %d\r\n", estadoVerde );
+         delay(50);
+      }
+
+      if( tec4_act == OFF && tec4_ant == ON ) {
+         estadoAzul = !estadoAzul;
+         printf( "Azul: %d\r\n", estadoAzul );
+         delay(50);
+      }
+
+      tec1_ant = tec1_act;
+      tec2_ant = tec2_act;
+      tec3_ant = tec3_act;
+      tec4_ant = tec4_act;
+
+      gpioWrite( LEDR, estadoRojo  && estadoMaestro );
+      gpioWrite( LEDG, estadoVerde && estadoMaestro );
+      gpioWrite( LEDB, estadoAzul  && estadoMaestro );
+
+      delay(10);
+   }
+
+   return 0;
 }
